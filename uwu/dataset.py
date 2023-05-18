f58ead77571e46aa8497be08cd4307d2c898e471
Zai <130903099+zaiYellYintAung@users.noreply.github.com>
2023-05-18 06:41:13 +0630

    starting model

--- uwu/dataset.py
+++ uwu/dataset.py
@@ -1,3 +1,15 @@
-class Conversations():
-    def __init__(self) -> None:
+import torch
+import torchaudio
+
+
+class Conversations(torch.utils.data.Dataset):
+    def __init__(self)
+        super().__init__()
+        
+
+    def __getitem__(self):
         pass
+
+    def __len__(self):
+        pass
+
--- uwu/models.py
+++ uwu/models.py
@@ -1,6 +1,11 @@
 from torch import nn
 import torch
+import torchaudio
 
 class Generator(nn.Module):
     def __init__(self):
         pass
+
+    def forward(self,x):
+        pass
+
