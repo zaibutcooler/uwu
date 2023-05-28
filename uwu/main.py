acb3ab3192f867b95ef0b5481eb8da065edcca19
Zai <130903099+zaiYellYintAung@users.noreply.github.com>
2023-05-28 07:42:32 +0630

    uwu params init

--- uwu/main.py
+++ uwu/main.py
@@ -1,15 +1,24 @@
+import torch
+from dataset import Conversations
+from models import Generator
+
 class Uwu:
-    def __init__(self):
-        pass
+    def __init__(self,num_epoch=20,lr=0.01,):
+
+        self.num_epoch = num_epoch
+        self.lr = lr
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.dataset = Conversations()
+        self.model = Generator().to(self.device)
+
 
     def generate(self,texts):
         pass
 
     def train(self):
-        pass
+        optimizer = torch.optim.Adam(self.model.parameters(),lr=self.lr)
+        for epoch in range(self.num_epoch):
+            for i,(x,y) in enumerate(self.dataset):
+                optimizer.zero_grad()
 
-    def sample(self):
-        pass
-
-    def load_pretrained(self):
-        pass
+            print(f"Epoch {epoch} done...")
